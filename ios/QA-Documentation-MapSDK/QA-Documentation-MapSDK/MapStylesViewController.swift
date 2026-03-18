81cf52245c2c5c26964dcd0b63f5812b985c4e13
Ashley Kennedy <144921101+akennedyTrimble@users.noreply.github.com>
2026-03-18 12:32:03 -0400

    Update iOS Maps Sample App for 2.1.0 iOS Maps release

--- ios/QA-Documentation-MapSDK/QA-Documentation-MapSDK/MapStylesViewController.swift
+++ ios/QA-Documentation-MapSDK/QA-Documentation-MapSDK/MapStylesViewController.swift
@@ -50,101 +50,28 @@ class MapStylesViewController : UIViewController, AccountManagerDelegate {
             let dayButton = addButton(title: "MOBILE_DAY", action: #selector(dayButtonPressed))
             let nightButton = addButton(title: "MOBILE_NIGHT", action: #selector(nightButtonPressed))
             let mobileSatelliteButton = addButton(title: "MOBILE_SATELLITE", action: #selector(mobileSatelliteButtonPressed))
-            let terrainButton = addButton(title: "TERRAIN", action: #selector(terrainButtonPressed))
-            let transportationButton = addButton(title: "TRANSPORTATION", action: #selector(transportationButtonPressed))
-            let basicButton = addButton(title: "BASIC", action: #selector(basicButtonPressed))
-            let dataDarkButton = addButton(title: "DATADARK", action: #selector(dataDarkButtonPressed))
-            let dataLightButton = addButton(title: "DATALIGHT", action: #selector(dataLightButtonPressed))
-            let defaultButton = addButton(title: "DEFAULT", action: #selector(defaultButtonPressed))
-            let mobileDefaultButton = addButton(title: "MOBILE_DEFAULT", action: #selector(mobileDefaultButtonPressed))
-            let satelliteButton = addButton(title: "SATELLITE", action: #selector(satelliteButtonPressed))
             
             NSLayoutConstraint.activate([
-                satelliteButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -10),
-                satelliteButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                
-                mobileDefaultButton.bottomAnchor.constraint(equalTo: satelliteButton.topAnchor, constant: -5),
-                mobileDefaultButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                mobileDefaultButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                defaultButton.bottomAnchor.constraint(equalTo: mobileDefaultButton.topAnchor, constant: -5),
-                defaultButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                defaultButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                dataLightButton.bottomAnchor.constraint(equalTo: defaultButton.topAnchor, constant: -5),
-                dataLightButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                dataLightButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                dataDarkButton.bottomAnchor.constraint(equalTo: dataLightButton.topAnchor, constant: -5),
-                dataDarkButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                dataDarkButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                basicButton.bottomAnchor.constraint(equalTo: dataDarkButton.topAnchor, constant: -5),
-                basicButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                basicButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                transportationButton.bottomAnchor.constraint(equalTo: basicButton.topAnchor, constant: -5),
-                transportationButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                transportationButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                terrainButton.bottomAnchor.constraint(equalTo: transportationButton.topAnchor, constant: -5),
-                terrainButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                terrainButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
-                
-                mobileSatelliteButton.bottomAnchor.constraint(equalTo: terrainButton.topAnchor, constant: -5),
+                mobileSatelliteButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -5),
                 mobileSatelliteButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                mobileSatelliteButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
                 
                 nightButton.bottomAnchor.constraint(equalTo: mobileSatelliteButton.topAnchor, constant: -5),
                 nightButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                nightButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
+                nightButton.leadingAnchor.constraint(equalTo: mobileSatelliteButton.leadingAnchor, constant: 0),
                 
                 dayButton.bottomAnchor.constraint(equalTo: nightButton.topAnchor, constant: -5),
                 dayButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -10),
-                dayButton.leadingAnchor.constraint(equalTo: satelliteButton.leadingAnchor, constant: 0),
+                dayButton.leadingAnchor.constraint(equalTo: mobileSatelliteButton.leadingAnchor, constant: 0),
             ])
         }
     @objc func dayButtonPressed() {
-        mapView.styleURL = TMGLStyle.mobileDayStyleURL
+        mapView.styleURL = TMGLTrimbleMobileStyle.mobileDayStyleURL
     }
     @objc func nightButtonPressed() {
-        mapView.styleURL = TMGLStyle.mobileNightStyleURL
+        mapView.styleURL = TMGLTrimbleMobileStyle.mobileNightStyleURL
     }
     @objc func mobileSatelliteButtonPressed() {
-        mapView.styleURL = TMGLStyle.mobileSatelliteStyleURL
-    }
-    
-    @objc func terrainButtonPressed() {
-        mapView.styleURL = TMGLStyle.terrainStyleURL
-    }
-    
-    @objc func transportationButtonPressed() {
-        mapView.styleURL = TMGLStyle.transportationStyleURL
-    }
-    
-    @objc func basicButtonPressed() {
-        mapView.styleURL = TMGLStyle.basicStyleURL
-    }
-    
-    @objc func dataDarkButtonPressed() {
-        mapView.styleURL = TMGLStyle.dataDarkStyleURL
-    }
-    
-    @objc func dataLightButtonPressed() {
-        mapView.styleURL = TMGLStyle.dataLightStyleURL
-    }
-    
-    @objc func defaultButtonPressed() {
-        mapView.styleURL = TMGLStyle.defaultStyleURL
-    }
-    
-    @objc func mobileDefaultButtonPressed() {
-        mapView.styleURL = TMGLStyle.mobileDefaultStyleURL
-    }
-    
-    @objc func satelliteButtonPressed() {
-        mapView.styleURL = TMGLStyle.satelliteStyleURL
-    }
-    
+        mapView.styleURL = TMGLTrimbleMobileStyle.mobileSatelliteStyleURL
+    }  
 }
 
